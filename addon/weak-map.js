6f13f1592c2aaa516b06662307d16116526f488b
Travis Hoover <thoov7@gmail.com>
2015-09-17 11:25:52 -0700

    Improve WeakMap construction and default Ember.WeakMap

--- addon/weak-map.js
+++ addon/weak-map.js
@@ -7,78 +7,85 @@ var {
 } = Ember;
 
 var id      = 0;
-var metaKey = '_weak';
+var dateKey = new Date().getTime();
+var metaKey = symbol();
 
 function UNDEFINED() {}
 
-function WeakMap() {
-  this._id = `__ember${new Date().getTime()}${id++}`;
+function symbol() {
+  return `__ember${dateKey}${id++}`;
 }
 
-/*
- * @method get
- * @param key {Object}
- * @return {*} stored value
- */
-WeakMap.prototype.get = function(obj) {
-  var metaInfo          = meta(obj);
-  var metaObject        = metaInfo[metaKey];
+class WeakMap {
+  constructor() {
+    this._id = symbol();
+  }
 
-  if (metaInfo && metaObject) {
-    if (metaObject[this._id] === UNDEFINED) {
-      return undefined;
+  /*
+   * @method get
+   * @param key {Object}
+   * @return {*} stored value
+   */
+  get(obj) {
+    var metaInfo   = meta(obj);
+    var metaObject = metaInfo[metaKey];
+
+    if (metaInfo && metaObject) {
+      if (metaObject[this._id] === UNDEFINED) {
+        return undefined;
+      }
+
+      return metaObject[this._id];
+    }
+  }
+
+  /*
+   * @method set
+   * @param key {Object}
+   * @param value {Any}
+   * @return {Any} stored value
+   */
+  set(obj, value) {
+    assert('Uncaught TypeError: Invalid value used as weak map key', typeOf(obj) === 'object');
+    var metaInfo = meta(obj);
+
+    if (value === undefined) {
+      value = UNDEFINED;
     }
 
-    return metaObject[this._id];
-  }
-};
+    if (!metaInfo[metaKey]) {
+      metaInfo[metaKey] = {};
+    }
 
-/*
- * @method set
- * @param key {Object}
- * @param value {Any}
- * @return {Any} stored value
- */
-WeakMap.prototype.set = function(obj, value) {
-  assert('Uncaught TypeError: Invalid value used as weak map key', typeOf(obj) === 'object');
-  var metaInfo = meta(obj);
-
-  if (value === undefined) {
-    value = UNDEFINED;
+    metaInfo[metaKey][this._id] = value;
+    return this;
   }
 
-  if (!metaInfo[metaKey]) {
-    metaInfo[metaKey] = {};
+  /*
+   * @method has
+   * @param key {Object}
+   * @return {Boolean} if the key exists
+   */
+  has(obj) {
+    var metaInfo          = meta(obj);
+    var metaObject        = metaInfo[metaKey];
+
+    return (metaObject && metaObject[this._id] !== undefined);
   }
 
-  metaInfo[metaKey][this._id] = value;
-  return this;
-};
+  /*
+   * @method delete
+   * @param key {Object}
+   */
+  delete(obj) {
+    var metaInfo = meta(obj);
 
-/*
- * @method has
- * @param key {Object}
- * @return {Boolean} if the key exists
- */
-WeakMap.prototype.has = function(obj) {
-  var metaInfo          = meta(obj);
-  var metaObject        = metaInfo[metaKey];
+    if (this.has(obj)) {
+      delete metaInfo[metaKey][this._id];
+    }
 
-  return (metaObject && metaObject[this._id] !== undefined);
-};
-
-/*
- * @method delete
- * @param key {Object}
- */
-WeakMap.prototype.delete = function(obj) {
-  var metaInfo = meta(obj);
-
-  if (this.has(obj)) {
-    delete metaInfo[metaKey][this._id];
+    return this;
   }
+}
 
-  return this;
-};
-
-export default WeakMap;
+export default (Ember.WeakMap) ? Ember.WeakMap : WeakMap;
