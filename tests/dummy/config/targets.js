dd478ad64f5d799916d8dbb39e47e3e4b494d665
Travis Hoover <thoov7@gmail.com>
2018-02-22 11:10:11 -0800

    [IMPR] Upgrade to 3.0 and add OVERRIDE_WEAKMAP
    window.OVERRIDE_WEAKMAP is needed for the testing case where we
    include the polyfill version but WeakMap is present. In this case
    we need a flag to override the default behavior of using window.WeakMap.

--- tests/dummy/config/targets.js
+++ tests/dummy/config/targets.js
@@ -1,6 +1,16 @@
-/* eslint-env node */
+'use strict'
+
+const browsers = [
+  '> 1%'
+];
+
+const isCI = !!process.env.CI;
+const isProduction = process.env.EMBER_ENV === 'production';
+
+if (isCI || isProduction) {
+  browsers.push('ie 11');
+}
+
 module.exports = {
-  browsers: [
-    '> 1%'
-  ]
+  browsers
 };
